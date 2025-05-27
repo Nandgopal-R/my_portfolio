8902f6e911dd12fea9bd9aad40a24e0158d215b5
Nandgopal-R <nandgopalrnair@gmail.com>
2025-05-27 22:07:28 +0530

    correct order of education

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -3,6 +3,18 @@ import type { Education } from '../types';
 
 export const items: Array<Education> = [
   {
+    degree: 'Bacherlors Degree in Computer Science',
+    description: '',
+    location: 'Coimbator, Tamil Nadu',
+    logo: Assets.Unknown,
+    name: '',
+    organization: 'Amrita Vishwa Vidhyapeetham',
+    period: { from: new Date(2023, 5, 2), to: new Date(2027, 5, 1) },
+    shortDescription: '',
+    slug: 'dummy-education-item-2',
+    subjects: ['Python', 'Java', 'C', 'C++',]
+  },
+  {
     degree: '12th Boards',
     description: '',
     location: 'Chennai, Tamil Nadu',
@@ -14,18 +26,6 @@ export const items: Array<Education> = [
     slug: 'dummy-education-item',
     subjects: ['Python', 'SQL']
   },
-  {
-    degree: 'Bacherlors Degree in Computer Science',
-    description: '',
-    location: 'Coimbator, Tamil Nadu',
-    logo: Assets.Unknown,
-    name: '',
-    organization: 'Amrita Vishwa Vidhyapeetham',
-    period: { from: new Date(2023, 5, 2), to: new Date(2027, 5, 1) },
-    shortDescription: '',
-    slug: 'dummy-education-item-2',
-    subjects: ['Python', 'Java', 'C', 'C++',]
-  }
 ];
 
 export const title = 'Education';
